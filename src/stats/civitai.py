02c0a9df379cea29a6b5c52fd4181d6a08a0c432
shihaoyu <shihaoyu@echo.tech>
2024-11-13 12:23:02 +0800

    update civitai api

--- src/stats/civitai.py
+++ src/stats/civitai.py
@@ -1,5 +1,7 @@
 import requests
-from datetime import datetime
+import json
+import urllib.parse
+from datetime import datetime, timedelta
 
 from plombery import task, get_logger
 
@@ -9,49 +11,42 @@ from plombery import task, get_logger
 # base_models: ["Flux.1 D", "Flux.1 S"]
 def get_civitai_models(period, cursor, base_models):
     params = {
-        "limit": 100,
-        "cursor": cursor,
-        "types": [
-            "Checkpoint",
-            "TextualInversion",
-            "Hypernetwork",
-            "AestheticGradient",
-            "Upscaler",
-            "MotionModule",
-            "Other",
-            "Controlnet",
-            "DoRA",
-            "Wildcards",
-            "LoCon",
-            "Poses",
-            "VAE",
-            "LORA"
-        ],
-        "sort": "Newest",
-        "period": period,
-        "periodMode": "published",
-        "token": "58f763daff342bdb95f67299aabc4753",
-        "baseModels": base_models
+        "json": {
+            "period": period,
+            "periodMode": "published",
+            "sort": "Newest",
+            "types": ["Checkpoint", "TextualInversion", "Hypernetwork", "AestheticGradient", "Upscaler", "Controlnet", "DoRA", "LoCon", "LORA", "MotionModule", "VAE", "Poses", "Wildcards", "Other"],
+            "baseModels": base_models,
+            "pending": False,
+            "browsingLevel": 31,
+            "cursor": cursor,
+            "authed": True
+        }
     }
-    response = requests.get("https://civitai.com/api/v1/models", params=params)
+    params_str = json.dumps(params)
+    params_str = urllib.parse.quote_plus(params_str)
+    response = requests.get("https://civitai.com/api/trpc/model.getAll?input="+params_str)
     if not response.ok:
         return [], None
 
     results = []
     cursor = None
-    if 'items' in response.json():
-        results = response.json()['items']
-    if 'metadata' in response.json() and 'nextCursor' in response.json()['metadata']:
-        cursor = response.json()['metadata']['nextCursor']
+    if 'result' in response.json() and 'data' in response.json()['result'] and 'json' in response.json()['result']['data']:
+        if 'items' in response.json()['result']['data']['json']:
+            results = response.json()['result']['data']['json']['items']
+        if 'nextCursor' in response.json()['result']['data']['json']:
+            cursor = response.json()['result']['data']['json']['nextCursor']
     return results, cursor
 
 
 def count_civitai_models(period, base_models):
-    cursor = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    cursor = (datetime.now() + timedelta(days=1)
+              ).strftime("%Y-%m-%dT%H:%M:%S.000Z")
     total_count = 0
     while True:
         models, cursor = get_civitai_models(period, cursor, base_models)
         total_count += len(models)
+        print("new models: ", len(models), "total count: ", total_count)
         if len(models) == 0 or cursor is None:
             break
 
@@ -60,8 +55,15 @@ def count_civitai_models(period, base_models):
 
 @task
 def count_civitai_models_task():
+    logger = get_logger()
+
     flux_count =  count_civitai_models("AllTime", ["Flux.1 D", "Flux.1 S"])
-    sd35_count = count_civitai_models("AllTime", ["SD 3.5", "SD 3.5 Medium", "SD 3.5 Large", "SD 3.5 Large Turbo"])
+    logger.info("flux count: %d", flux_count)
+
+    sd35_count = count_civitai_models(
+        "AllTime", ["SD 3.5", "SD 3.5 Medium", "SD 3.5 Large", "SD 3.5 Large Turbo"])
+    logger.info("sd35 count: %d", sd35_count)
+
     return [
         {
             'base model': 'Flux',
